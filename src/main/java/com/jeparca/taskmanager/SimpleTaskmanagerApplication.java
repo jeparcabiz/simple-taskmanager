bb10388f12bf1f69b89877a96dda8d5121e46178
Jesus Pardo <jesus.pc.bt@gmail.com>
2024-06-02 09:28:25 +0200

    Initial commit with Spring app skeleton

---
+++ src/main/java/com/jeparca/taskmanager/SimpleTaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.jeparca.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeparca/taskmanager/SimpleTaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeparca.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
