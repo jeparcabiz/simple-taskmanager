ef1bbe61a6742d1e9462c1f9ad1245a039666496
Jesus Pardo <jesus.pc.bt@gmail.com>
2024-06-02 10:26:24 +0200

    Added repository to application layer

---
+++ src/main/java/com/jeparca/taskmanager/application/port/out/TaskRepositoryPort.java
@@ -0,0 +1,7 @@
+package com.jeparca.taskmanager.application.port.out;
+
+import com.jeparca.taskmanager.domain.repository.TaskRepository;
+
+public interface TaskRepositoryPort extends TaskRepository {
+
+}
